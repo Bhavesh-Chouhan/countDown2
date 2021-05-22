3d42c914177842a15128fabb8dc1663299b9d56d
Bhavesh Chouhan <bhaveshchouhan645@gmail.com>
2021-05-22 10:24:19 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const countdown = () => {
+  const limitDate = new Date(2021, 4, 17, 00, 00, 00).getTime();
+  console.log(limitDate);
+
+  const dateToday = new Date().getTime();
+  const gap = limitDate - dateToday;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  document.querySelector(".day").innerHTML = textDay;
+  document.querySelector(".hour").innerHTML = textHour;
+  document.querySelector(".minute").innerHTML = textMinute;
+  document.querySelector(".second").innerHTML = textSecond;
+};
+
+setInterval(countdown, 1000);
